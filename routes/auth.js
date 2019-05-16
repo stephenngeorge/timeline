807476f9e8c6fe67846942c204018c21629dd963
Stephen George <stephenngeorge@gmail.com>
2019-05-16 15:59:32 +0100

    jwt implementation on login

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 // import models
 import { User } from '../models'
@@ -44,33 +45,61 @@ router.post('/signup', async (req, res, next) => {
     // check username availability
     const checkUsername = await User.find({ username: req.body.username })
     if (checkUsername.length !== 0) {
-        res.status(400).json({
+        return res.status(400).json({
             type: "ERROR",
             message: `username: ${req.body.username} is taken`
         })
     }
-        
     // create user if username is available
-    else {
-        // hash password
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        // create new user
-        const user = await new User({ ...req.body, password: hashedPassword }).save()
-        try {
-            res.json({
-                type: "CREATE",
-                message: `created new user: ${user.username}`,
-                data: user
-            })
-        }
-        catch(e) {
-            res.status(400).json({
-                type: "ERROR",
-                message: `failed to create user: ${req.body.username}`,
-                e
-            })
-        }
+    // hash password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    // create new user
+    const user = await new User({ ...req.body, password: hashedPassword }).save()
+    try {
+        res.json({
+            type: "CREATE",
+            message: `created new user: ${user.username}`,
+            data: user
+        })
     }
+    catch(e) {
+        res.status(400).json({
+            type: "ERROR",
+            message: `failed to create user: ${req.body.username}`,
+            e
+        })
+    }
+})
+
+// USER LOGIN, RETURNING JWT & USER OBJECT
+router.post('/login', async (req, res, next) => {
+    const user = await User.find({ username: req.body.username })
+    if (user.length === 0) {
+        return res.status(401).json({
+            type: "ERROR",
+            message: `failed to find user with username: ${req.body.username}`
+        })
+    }
+    // continue login if user is found
+    // compare passwords
+    const checkPassword = await bcrypt.compare(req.body.password, user[0].password)
+    if (!checkPassword) {
+        return res.status(401).json({
+            type: "ERROR",
+            message: "password did not match our records..."
+        })
+    }
+    // if correct password, return user & JWT
+    const token = jwt.sign({
+        username: user[0].username,
+        userId: user[0]._id
+    }, process.env.JWT_KEY, { expiresIn: '1h' })
+    res.status(200).json({
+        type: "LOGIN",
+        message: `logged in user: ${user[0].username}`,
+        token,
+        data: user[0]
+    })
 })
 
 // UPDATE SINGLE USER BY ID
