e31d74f362644e780f426d017db7f796454f005b
Stephen George <stephenngeorge@gmail.com>
2019-05-18 10:16:20 +0100

    created auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,141 @@
+import { User } from '../models'
+import { validate } from '../utils'
+
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+// GET USER COUNT
+export const userCount = async (req, res, next) => {
+    try {
+        const users = await User.find()
+        res.send(`user count: ${users.length}`)
+    }
+    catch(e) {
+        res.status(400).json({
+            type: "ERROR",
+            message: "failed to fetch users"
+        })
+    }
+}
+// GET SINGLE USER BY ID
+export const getSingleUser = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ _id: req.params.id }).populate('timelines').exec()
+        res.json({
+            type: "READ",
+            message: `found user: ${user.username}`,
+            data: user
+        })
+    }
+    catch(e) {
+        res.status(400).json({
+            type: "ERROR",
+            message: `failed to find user: ${req.params.id}`
+        })
+    }
+}
+
+// CREATE NEW USER
+export const userSignUp = async (req, res, next) => {
+    try {
+        // check username availability
+        const checkUsername = await User.find({ username: req.body.username })
+        if (checkUsername.length !== 0) {
+            return res.status(400).json({
+                type: "ERROR",
+                message: `username: ${req.body.username} is taken`
+            })
+        }
+        // create user if username is available
+        // validate & hash password
+        const validPassword = validate(req.body.password)
+        if (!validPassword) {
+            return res.status(400).json({
+                type: "ERROR",
+                message: "invalid password"
+            })
+        }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    
+        // create new user
+        const user = await new User({ ...req.body, password: hashedPassword }).save()
+        res.json({
+            type: "CREATE",
+            message: `created new user: ${user.username}`,
+            data: user
+        })
+    }
+    catch(e) {
+        res.status(400).json({
+            type: "ERROR",
+            message: `failed to create user: ${req.body.username}`
+        })
+    }
+}
+// LOGIN USER WITH USERNAME & PASSWORD
+export const userLogin = async (req, res, next) => {
+    const user = await User.find({ username: req.body.username })
+    if (user.length === 0) {
+        return res.status(401).json({
+            type: "ERROR",
+            message: `failed to find user with username: ${req.body.username}`
+        })
+    }
+    // continue login if user is found
+    // compare passwords
+    const checkPassword = await bcrypt.compare(req.body.password, user[0].password)
+    if (!checkPassword) {
+        return res.status(401).json({
+            type: "ERROR",
+            message: "password did not match our records..."
+        })
+    }
+    // if correct password, return user & JWT
+    const token = jwt.sign({
+        username: user[0].username,
+        userId: user[0]._id
+    }, process.env.JWT_KEY, { expiresIn: '1h' })
+    res.status(200).json({
+        type: "LOGIN",
+        message: `logged in user: ${user[0].username}`,
+        token,
+        data: user[0]
+    })
+}
+
+// UPDATE SINGLE USER BY ID
+export const updateSingleUser = async (req, res, next) => {
+    try {
+        const updatedUser = await User.findOneAndUpdate({ _id: req.params.id }, {...req.body, updated_at: Date.now()}, { new: true })
+        res.json({
+            type: "UPDATE",
+            message: `updated user: ${updatedUser.username}`,
+            data: updatedUser
+        })
+    }
+    catch(e) {
+        res.status(400).json({
+            type: "ERROR",
+            message: "failed to update user"
+        })
+    }
+}
+
+export const deleteSingleUser = async (req, res, next) => {
+    try {
+        // DELETE TIMELINES AND NODES FOR THIS USER
+        const deletedUser = await User.findOneAndDelete({ _id: req.params.id })
+        
+        res.json({
+            type: "DELETE",
+            message: `deleted user: ${deletedUser.username}`,
+            data: deletedUser
+        })
+    }
+    catch(e) {
+        res.status(400).json({
+            type: "ERROR",
+            message: "failed to delete user"
+        })
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,159 +1,25 @@
 import { Router } from 'express'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-
-import { verifyAuth, verifyPassword } from '../middleware'
-import { validate } from '../utils'
-
-// import models
-import { Node, Timeline, User } from '../models'
-
 const router = new Router()
 
-// GET USER COUNT
-router.get('/', async (req, res, next) => {
-    const users = await User.find()
-    try {
-        res.send(`user count: ${users.length}`)
-    }
-    catch(e) {
-        res.status(400).json({
-            type: "ERROR",
-            message: "failed to fetch users"
-        })
-    }
-})
+// import middleware
+import { verifyAuth, verifyPassword } from '../middleware'
+// import controllers
+import { authController } from '../controllers'
 
+// GET USER COUNT
+router.get('/', authController.userCount)
 // GET SINGLE USER BY ID
-router.get('/:id', async (req, res, next) => {
-    try {
-        const user = await User.findOne({ _id: req.params.id }).populate('timelines').exec()
-        res.json({
-            type: "READ",
-            message: `found user: ${user.username}`,
-            data: user
-        })
-    }
-    catch(e) {
-        res.status(400).json({
-            type: "ERROR",
-            message: `failed to find user: ${req.params.id}`
-        })
-    }
-})
+router.get('/:id', authController.getSingleUser)
 
 // CREATE NEW USER
-router.post('/signup', async (req, res, next) => {
-    try {
-        // check username availability
-        const checkUsername = await User.find({ username: req.body.username })
-        if (checkUsername.length !== 0) {
-            return res.status(400).json({
-                type: "ERROR",
-                message: `username: ${req.body.username} is taken`
-            })
-        }
-        // create user if username is available
-        // validate & hash password
-        const validPassword = validate(req.body.password)
-        if (!validPassword) {
-            return res.status(400).json({
-                type: "ERROR",
-                message: "invalid password"
-            })
-        }
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    
-        // create new user
-        const user = await new User({ ...req.body, password: hashedPassword }).save()
-        res.json({
-            type: "CREATE",
-            message: `created new user: ${user.username}`,
-            data: user
-        })
-    }
-    catch(e) {
-        res.status(400).json({
-            type: "ERROR",
-            message: `failed to create user: ${req.body.username}`
-        })
-    }
-})
-
+router.post('/signup', authController.userSignUp)
 // USER LOGIN, RETURNING JWT & USER OBJECT
-router.post('/login', async (req, res, next) => {
-    const user = await User.find({ username: req.body.username })
-    if (user.length === 0) {
-        return res.status(401).json({
-            type: "ERROR",
-            message: `failed to find user with username: ${req.body.username}`
-        })
-    }
-    // continue login if user is found
-    // compare passwords
-    const checkPassword = await bcrypt.compare(req.body.password, user[0].password)
-    if (!checkPassword) {
-        return res.status(401).json({
-            type: "ERROR",
-            message: "password did not match our records..."
-        })
-    }
-    // if correct password, return user & JWT
-    const token = jwt.sign({
-        username: user[0].username,
-        userId: user[0]._id
-    }, process.env.JWT_KEY, { expiresIn: '1h' })
-    res.status(200).json({
-        type: "LOGIN",
-        message: `logged in user: ${user[0].username}`,
-        token,
-        data: user[0]
-    })
-})
+router.post('/login', authController.userLogin)
 
 // UPDATE SINGLE USER BY ID
-router.put('/:id', verifyAuth, async (req, res, next) => {
-    try {
-        const updatedUser = await User.findOneAndUpdate({ _id: req.params.id }, {...req.body, updated_at: Date.now()}, { new: true })
-        res.json({
-            type: "UPDATE",
-            message: `updated user: ${updatedUser.username}`,
-            data: updatedUser
-        })
-    }
-    catch(e) {
-        res.status(400).json({
-            type: "ERROR",
-            message: "failed to update user"
-        })
-    }
-})
+router.put('/:id', verifyAuth, authController.updateSingleUser)
 
 // DELETE SINGLE USER BY ID
-router.delete('/:id', verifyAuth, verifyPassword, async (req, res, next) => {
-    try {
-        // find all timelines for this user
-        const timelines = await Timeline.find({ author: req.params.id })
-        timelines.forEach(timeline => {
-            // loop through and remove all nodes for each timeline
-            await Node.deleteMany({ timeline: timeline._id })
-            await Timeline.findOneAndDelete({ _id: timeline._id })
-        })
-        // delete user
-        const deletedUser = await User.findOneAndDelete({ _id: req.params.id })
-        
-        res.json({
-            type: "DELETE",
-            message: `deleted user: ${deletedUser.username}`,
-            data: deletedUser
-        })
-    }
-    catch(e) {
-        res.status(400).json({
-            type: "ERROR",
-            message: "failed to delete user"
-        })
-    }
-})
+router.delete('/:id', verifyAuth, verifyPassword, authController.deleteSingleUser)
 
 export default router
